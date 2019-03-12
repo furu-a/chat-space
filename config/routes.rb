0aa25b5e70cff2980638737d3101b8e155c90803
furu-a <croqkes@gmail.com>
2019-03-12 18:33:28 +0900

    routes_path_edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   devise_for :users
-  root 'messages#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'groups#index'
+
   resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :chat_space, only: [:index]
+  end
+
 
 end
