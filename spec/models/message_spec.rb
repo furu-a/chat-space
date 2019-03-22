ea6684378d399869598917ed3b7a52985c08e894
furu-a <croqkes@gmail.com>
2019-03-22 19:42:50 +0900

    add_messages_rspec

---
+++ spec/models/message_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+
+
+describe Message do
+  describe "#create" do
+    describe "valid type" do
+
+      it "is valid with empty image" do
+        expect(build(:message, image: nil)).to be_valid
+      end
+
+      it "is valid with empty content" do
+        expect(build(:message, content: nil)).to be_valid
+      end
+
+      it "is valid with a content and a image" do
+        message = build(:message)
+        message.valid?
+        expect(message).to be_valid
+      end
+    end
+
+    describe "invalid type" do
+
+      it "is valid without a content and a image" do
+        message = build(:message, content: nil, image: nil)
+        message.valid?
+        expect(message.errors[:content]).to require("を入力してください")
+      end
+
+      it "is valid without group_id" do
+        message = build(:message, group_id: nil)
+        message.valid?
+        expect(message.errors[:group]).to require("を入力してください")
+      end
+
+      it "is valid without user_id" do
+        message = build(:message, user_id: nil)
+        message.valid?
+        expect(message.errors[:user]).to require("を入力してください")
+      end
+    end
+
+  end
+end
