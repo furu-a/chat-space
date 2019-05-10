b71473880e2bb98a90977b1415d8b94960d76be0
furu-a <croqkes@gmail.com>
2019-05-10 22:00:23 +0900

    change_comment-out_to_text

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,26 +39,26 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :ssh_options, verify_host_key: :secure
 
 # config valid only for current version of Capistrano
-# lock :capistrano, '3.11.0'
+lock :capistrano, '3.11.0'
 
-# set :application, 'chat-space'
-# set :repo_url,  'git@github.com:furu-a/chat-space.git'
+set :application, 'chat-space'
+set :repo_url,  'git@github.com:furu-a/chat-space.git'
 
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-# set :rbenv_type, :user
-# set :rbenv_ruby, '2.3.1'
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
 
-# set :ssh_options, auth_methods: ['publickey'],
-#                   keys: ['~/.ssh/furu.pem']
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/furu.pem']
 
-# set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-# set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-# set :keep_releases, 5
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
 
-# after 'deploy:publishing', 'deploy:restart'
-# namespace :deploy do
-#   task :restart do
-#     invoke 'unicorn:restart'
-#   end
-# end
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
