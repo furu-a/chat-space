6b622f4b36ac6ccbe82464a987e9714d0f9a4ba9
furu-a <croqkes@gmail.com>
2019-05-14 19:21:48 +0900

    change_auto_deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,7 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
 
 
-# server '52.199.171.169', user: 'ec2-user', roles: %w{app db web}
+server '52.199.171.169', user: 'ec2-user', roles: %w{app db web}
 
 
 
